375588e9ba66b022d0c4255aa04d6fc667cfe4c4
ChancellorLam <135484498+ChancellorLam@users.noreply.github.com>
2023-08-22 07:48:00 -0400

    Added quadrature class for quadrature methods

---
+++ piApproximationMethods/src/Quadrature.java
@@ -0,0 +1,91 @@
+package piApproximationMethods;
+
+public class Quadrature {
+	
+	double simpsonsRule(int numSubintervals) {  // numerically integrate using Simpson's Rule
+		int numEndpoints = numSubintervals + 1;
+		double sumOfTerms = 0;
+		double subintervalWidth = 0.25 / (double) numSubintervals;
+		
+		 // the 0th and last terms in the calculation of the integral using Simpson's rule are not modified
+		 // but for all other terms, odd terms are multiplied by 4 and even terms are multiplied by 2
+		 // so an example calculation would be: f(0) + 4f(1) + 2f(2) + 4f(3) + ... f(n)
+		 // then all of this would be multiplied by width of the subintervals / 3
+		
+		sumOfTerms = sumOfTerms + Math.sqrt(0 - 0 * 0); // this is the 0th term
+		System.out.println("f(0): 0");
+		for (int i = 1; i < numEndpoints - 1; i++) { // modify and then add every term except for 0th and last
+			double x = subintervalWidth * i;
+			if (i % 2 == 1) { // odd terms
+				sumOfTerms = sumOfTerms + 4 * Math.sqrt(x - x * x);
+				System.out.println("+ 4f(" + x + "): " + 4 * Math.sqrt(x - x * x));
+			}
+			else if (i % 2 == 0) { // even terms
+				sumOfTerms = sumOfTerms + 2 * Math.sqrt(x - x * x);
+				System.out.println("+ 2f(" + x + "): " + 2 * Math.sqrt(x - x * x));
+			}
+		}
+		sumOfTerms = sumOfTerms + Math.sqrt(0.25 - 0.25 * 0.25); // add the last term
+		System.out.println("+ f(0.25): " + Math.sqrt(0.25 - 0.25 * 0.25));
+		
+		System.out.println();
+		System.out.println("Sum of terms: " + sumOfTerms);
+		System.out.println("Number of subintervals: " + numSubintervals);
+		System.out.println("Width of subintervals: " + subintervalWidth);
+		
+		// multiply sum of terms by width of subintervals / 3 to approximate integral with parabolas
+		double areaUnderCurve = sumOfTerms * subintervalWidth / 3;
+		return areaUnderCurve;
+	}
+	
+	double midpointRule(int numSubintervals) {  // numerically integrate using Midpoint Rule
+		double sumOfTerms = 0;
+		double subintervalWidth = 0.25 / (double) numSubintervals;
+		double midpointSize = subintervalWidth / 2;
+		double x = midpointSize; 
+		
+		for (int i = 0; i < numSubintervals; i++) {  // sum up all midpoints
+			sumOfTerms = sumOfTerms + Math.sqrt(x - x * x);
+			System.out.println("f(" + x + "): " + sumOfTerms);
+			x = x + subintervalWidth;
+		}
+		
+		System.out.println();
+		System.out.println("Sum of terms: " + sumOfTerms);
+		System.out.println("Number of subintervals: " + numSubintervals);
+		System.out.println("Width of subintervals: " + subintervalWidth);
+		
+		// multiply all midpoints by width of subintervals to approximate integral with rectangles
+		double areaUnderCurve = sumOfTerms * subintervalWidth;
+		return areaUnderCurve;
+	}
+	
+	double trapezoidalRule(int numSubintervals) {  // numerically integrate using Trapezoidal Rule
+		double sumOfTerms = 0;
+		double subintervalWidth = 0.25 / (double) numSubintervals;
+		
+		 // the 0th and last terms in the calculation of the integral using Trapezoidal rule are not modified
+		 // but all other terms are multiplied by 2
+		 // so an example calculation would be: f(0) + 2f(1) + 2f(2) + 2f(3) + ... f(n)
+		 // and then all of this would be multiplied by width of the subintervals / 2
+		
+		sumOfTerms = sumOfTerms + Math.sqrt(0 - 0 * 0); // this is the 0th term
+		System.out.println("f(0): 0");
+		for (int i = 1; i < numSubintervals; i++) { // modify every term except for 0th and last
+			double x = subintervalWidth * i;
+			sumOfTerms = sumOfTerms + 2 * Math.sqrt(x - x * x);
+			System.out.println("+ 2f(" + x + "): " + sumOfTerms);
+		}
+		sumOfTerms = sumOfTerms + Math.sqrt(0.25 - 0.25 * 0.25); // add the last term
+		System.out.println("+ f(0.25): " + sumOfTerms);
+		
+		System.out.println();
+		System.out.println("Sum of terms: " + sumOfTerms);
+		System.out.println("Number of subintervals: " + numSubintervals);
+		System.out.println("Width of subintervals: " + subintervalWidth);
+		
+		// multiply sum of terms by width of subintervals / 2 to approximate integral with trapezoids
+		double areaUnderCurve = sumOfTerms * subintervalWidth / 2;
+		return areaUnderCurve;
+	}
+}
