1f7917355601ec77aa85aa92d8c2f88938416bd5
ChancellorLam <135484498+ChancellorLam@users.noreply.github.com>
2023-08-22 03:54:59 -0400

    Created method to prevent InputMismatchException

--- piApproximationMethods/src/InterfaceLogic.java
+++ piApproximationMethods/src/InterfaceLogic.java
@@ -1,8 +1,35 @@
 package piApproximationMethods;
 import java.util.Scanner;
+import java.util.*;
 
 public class InterfaceLogic {
 	private static Scanner input = new Scanner(System.in);
+	
+	static long obtainInputFromUser(String question) {
+		long userInput = 0;
+		
+		while (userInput < 1) {
+			boolean errorMsg = false;
+			
+			System.out.println(question);
+			try {
+				userInput = input.nextLong();
+			}
+			catch (InputMismatchException e) {
+				errorMsg = true;
+				System.out.println("Invalid input. You must enter a positive integer that is at least 1.");
+				System.out.println();
+				input.nextLine();
+			}
+			
+			if (userInput < 1 && errorMsg == false) {
+				System.out.println("Invalid input. You must enter a positive integer that is at least 1.");
+				System.out.println();
+			}
+		}
+		return userInput;
+		
+	}
 	
 	static void approximatePi() {
 		boolean methodNotChosen = true;
@@ -38,9 +65,11 @@ public class InterfaceLogic {
 			}
 			else if (chosenMethod.isEmpty()) {
 				System.out.println("Invalid input. Please input a number from 0-4.");
+				System.out.println();
 			}
 			else {
 				System.out.println("Invalid input. Please input a number from 0-4.");
+				System.out.println();
 			}
 		}
 	}
